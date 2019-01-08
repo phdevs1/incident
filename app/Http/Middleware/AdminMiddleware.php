f1023f24d6c49c821592eafabf6d430f09a20658
pioh <pio.huisa@gmail.com>
2019-01-08 09:05:50 -0500

    d

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -15,6 +15,9 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
+        if (!auth()->check()) {
+            return redirect('login');
+        }
         if (auth()->user()->role!=0) {
             return redirect('home');
         }
---
+++ app/Models/Joder/Try.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Joder;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Try extends Model
+{
+    //
+}
